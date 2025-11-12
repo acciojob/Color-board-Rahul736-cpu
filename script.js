407b5a935ca0e6cf0b29a46d9e65e29f037ef9bc
Accio-1 <rahulwanjare291@gmail.com>
2025-11-12 15:49:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//your JS code here. If required.
+const container = document.getElementById("container");
+const boxCount = 800;
+
+for (let i = 0; i < boxCount; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+
+    square.addEventListener("mouseover", () => setColor(square));
+    square.addEventListener("mouseout", () => removeColor(square));
+
+    container.appendChild(square);
+}
+
+function setColor(element) {
+    const color = getRandomColor();
+    element.style.background = color;
+}
+
+function removeColor(element) {
+    setTimeout(() => {
+        element.style.background = "#1d1d1d";
+    }, 1000); 
+}
+
+function getRandomColor() {
+    return `hsl(${Math.random() * 360}, 100%, 50%)`; 
+}
